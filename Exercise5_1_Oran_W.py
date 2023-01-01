8306e8aa35bd237c4accd2d25871cf9b3fcb4a25
Jacobearth3 <121567799+Jacobearth3@users.noreply.github.com>
2023-01-02 01:20:14 +0700

    Added Coding Exercise5_1_Oran_W
    ทำโปรแกรมสำหรับประมวลค่าต่างๆจากตัวดำเนินการพื้นฐานด้วยตัวเลขที่ผู้ใช้ป้อนเข้ามา

---
+++ Exercise5_1_Oran_W.py
@@ -0,0 +1,8 @@
+#กำหนดการรับข้อมูลจากผู้ใช้
+num1 = int(input("First Number : "))
+num2 = int(input("Second Number : "))
+#แสดงผลข้อมูลต่างๆ ตามที่กำหนด
+print(num1," + ",num2,"=",num1+num2)
+print(num1," - ",num2,"=",num1-num2)
+print(num1," * ",num2,"=",num1*num2)
+print(num1," / ",num2,"=",num1/num2)
