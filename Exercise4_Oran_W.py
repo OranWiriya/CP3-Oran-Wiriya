e36c19bd09923850edecb9d2e2e294c30fd88270
Jacobearth3 <121567799+Jacobearth3@users.noreply.github.com>
2023-01-01 21:07:10 +0700

    Added Coding for exercise4
    Added coding for exercise4

---
+++ Exercise4_Oran_W.py
@@ -0,0 +1,11 @@
+#Exercise 4 ปฎิบัตการทดสอบตัวแปร
+#ตั้งค่าตัวแปรต่างๆ
+FoundationENG = 60.0
+GenBusiness = 75.0
+IntroComSystems = 80.0
+ComProgramming = 85.0
+print("---Your Score---")
+print("Foundation English : ",FoundationENG)
+print("General Business : ",GenBusiness)
+print("Introduction to Computer Systems : ",IntroComSystems)
+print("Computer Programming : ",ComProgramming)
