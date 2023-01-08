628fff557f9b2d4dfd2dcfb3f628cfe9ae799625
Jacobearth3 <121567799+Jacobearth3@users.noreply.github.com>
2023-01-08 18:02:31 +0700

    change method how to convent currency

--- Lecture114_Oran_W.py
+++ Lecture114_Oran_W.py
@@ -2,44 +2,48 @@
 # import libery to create widget and data Currency
 from tkinter import *
 from forex_python.converter import CurrencyRates
-from forex_python.bitcoin import BtcConverter
-
-
-def left_click_button(event):
-    if(text_box_selected == 1):
-        label_prefercurrency  = Label(main_window, text = "select first currency and second currency",font=("Helvetica",14))
-        label_prefercurrency.grid(row=5,columnspan=3)
-        text_box_selected_2 = Entry(main_window)
-        text_box_selected_2.grid(row=6,columnspan=3)
-        text_box_selected_3 = Entry(main_window)
-        text_box_selected_3.grid(row=7,columnspan=3)
-        result = currency_rate.get_rate(text_box_selected_2,text_box_selected_3)
-        label_result.configure(text = result)
-        label_result.grid(row=8,columnspan=3)
-        reset = ""
-        reset_button = Button(main_window,text = "reset",font=("Helvetica",14))
-        reset_button.grid(row=9,columnspan=3)
-        reset_button.bind("<Button-1>",reset = "0")
-        if(reset == "0"):
-            text_box_selected_2.destroy()
-            text_box_selected_3.destroy()
-            label_result.destroy()
-            reset_button.destroy()
-            return
-        
 
 currency_rate = CurrencyRates()
-bitcoin_rate = BtcConverter()
+currency =currency_rate.get_rates("USD")
+
+def getcurrency_list(currency):
+    currency_list = ["USD"]
+    for key in currency.keys():
+        currency_list.append(key)
+    return currency_list
+
+def calculate_currency(event):
+    result = currency_rate.get_rate("USD","THB")
+    label_result.configure(text = result)
+    label_result.grid(row=8,columnspan=3)
+
+def left_click_button(event):
+    pass
+
+currency_list = getcurrency_list(currency)              
+
 main_window = Tk()
-label_welcome = Label(main_window, text = "Welcome to Currencycoverter Program\n Please select your program",font=("Helvetica",18))
+label_welcome = Label(main_window, text = "Welcome to Currencycoverter Program\n Currency Exchange rate",font=("Helvetica",18))
 label_welcome.grid(row=0,columnspan=3)
-label_currencyrates = Label(main_window, text="1.Currency Exchangerate",font=("Helvetica",14))
-label_currencyrates.grid(row=2,columnspan=3)
-label_currencyrates = Label(main_window, text="2.Bitcoin Exchangerate",font=("Helvetica",14))
-label_currencyrates.grid(row=3,columnspan=3)
-text_box_selected = Entry(main_window)
-text_box_selected.grid(row=4,column=0,columnspan=3)
-calculate_button = Button(main_window, text = "select")
-calculate_button.bind("<Button-1>", left_click_button)
-label_result = Label(main_window,text="")
+first_currency = StringVar(main_window)
+first_currency.set(currency_list[0])  # default value
+second_currency = StringVar(main_window)
+second_currency.set(currency_list[0])  # default value
+dropdown_first_currency = OptionMenu(main_window, first_currency, *currency_list)
+dropdown_first_currency.grid(row=2,column=0)
+dropdown_second_currency = OptionMenu(main_window, second_currency, *currency_list)
+dropdown_second_currency.grid(row=5,column=0)
+label_currencyrates_first = Label(main_window, text="First currency",font=("Helvetica",14))
+label_currencyrates_first.grid(row=2,column=1)
+input_box_amount = Entry(main_window)
+input_box_amount.grid(row=4,column=0,columnspan=3)
+label_currencyrates_second = Label(main_window, text="Second currency",font=("Helvetica",14))
+label_currencyrates_second.grid(row=5,column=1)
+label_result = Label(main_window, text="จำนวนเงิน")
+label_result.grid(row=6, columnspan=3)
+label_currency_rate = Label(main_window, text="")
+label_currency_rate.grid(row=7, columnspan=3)
+calculated_button = Button(main_window, text = "Exchange!!")
+calculated_button.grid(row=9,columnspan=3)
+calculated_button.bind("<Button-1>", left_click_button)
 main_window.mainloop()
