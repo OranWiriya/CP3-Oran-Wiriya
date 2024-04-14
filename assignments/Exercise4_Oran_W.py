8107165c4c4405506ae82d258eae05973b4fb749
OranWiriya <121567799+OranWiriya@users.noreply.github.com>
2024-04-14 17:04:27 +0700

    commit exercise_4 borntodev

---
+++ assignments/Exercise4_Oran_W.py
@@ -0,0 +1,12 @@
+#Exercise 4 ปฎิบัตการทดสอบตัวแปร
+#ตั้งค่าตัวแปรต่างๆ
+foundationEng = 80.0
+genBusiness = 70.0
+introComSystems = 60.0
+comProgramming = 90.0
+#พิมพ์ค่าต่างๆออกมา
+print("--- Your Score ---")
+print("Foundation English : ",foundationEng)
+print("General Business : ",genBusiness)
+print("Introduction to Computer Systems : ",introComSystems)
+print("Computer Programming : ",comProgramming)
